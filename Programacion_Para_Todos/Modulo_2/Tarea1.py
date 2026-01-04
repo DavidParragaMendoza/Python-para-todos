b1c8f482d73ad29dc79c5c6e11343a0941955250
David Parraga Mendoza <davidparragamendoza@gmail.com>
2026-01-04 18:00:53 -0500

    Mi primer ejercicio del módulo 2

---
+++ Programacion_Para_Todos/Modulo_2/Tarea1.py
@@ -0,0 +1,12 @@
+'''
+Instala Python y un editor de texto de programación.
+
+Escribe un programa que imprima una línea que no sea "hola mundo"
+
+Haz dos capturas de pantalla:
+
+tu editor de texto mostrando el código, y
+
+tu línea de comandos mostrando el comando que ejecutaste y la salida del programa.
+'''
+print("¡Esta es mi primera tarea en Python!")
