43fdab7ec202e242605aaa38af0545674ff816ab
David Parraga Mendoza <davidparragamendoza@gmail.com>
2026-01-04 18:14:05 -0500

    Tarea 1 - modulo 2

--- Programacion_Para_Todos/Modulo_2/Tarea1.py
+++ Programacion_Para_Todos/Modulo_2/Tarea1.py
@@ -10,3 +10,5 @@ tu editor de texto mostrando el código, y
 tu línea de comandos mostrando el comando que ejecutaste y la salida del programa.
 '''
 print("¡Esta es mi primera tarea en Python!")
+print("Hola soy David Parraga, este es el inicio de mi aventura con Python.")
+print("la cual voy a documentar y publicar en redes. ")
